74ce8513a8d9f05ca07bca94487d464857019bcb
leon <18955993726@163.com>
2019-01-15 22:33:58 +0800

    多线程

---
+++ Concurrence.cpp
@@ -0,0 +1,17 @@
+#include <thread>
+using namespace std;
+void f()
+{
+    cout << "Hello ";
+}
+struct F
+{
+    void operator()() { cout << "Parallel World!\n"; }
+};
+void user()
+{
+    thread t1{f};
+    thread t2{F()};
+    t1.join();
+    t2.join();
+}
